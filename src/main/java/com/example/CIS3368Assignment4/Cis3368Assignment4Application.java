3295601f6621fe5f0f2be64b89e888f5287fe07a
gandhiUH <gvelusamy@uh.edu>
2020-04-11 20:04:38 -0500

    Initial commit

---
+++ src/main/java/com/example/CIS3368Assignment4/Cis3368Assignment4Application.java
@@ -0,0 +1,13 @@
+package com.example.CIS3368Assignment4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cis3368Assignment4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cis3368Assignment4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CIS3368Assignment4/Cis3368Assignment4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CIS3368Assignment4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cis3368Assignment4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
